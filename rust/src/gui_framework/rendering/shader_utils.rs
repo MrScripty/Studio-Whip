7abeee43315660bf6f7079f952501c1023f85e47
MrScripty <TheEnvironmentGuy@protonmail.com>
2025-03-26 08:51:13 -0400

    Made paths cross-platform
    For windows / linux interopt

--- rust/src/gui_framework/rendering/shader_utils.rs
+++ rust/src/gui_framework/rendering/shader_utils.rs
@@ -1,10 +1,12 @@
 use ash::vk;
 use std::fs;
+use std::path::PathBuf;
 use std::marker::PhantomData;
 
 pub fn load_shader(device: &ash::Device, filename: &str) -> vk::ShaderModule {
-    let shader_path = format!("./shaders/{}", filename);
-    let shader_code = fs::read(&shader_path).expect(&format!("Failed to read shader file: {}", shader_path));
+    let mut shader_path = PathBuf::from("./shaders");
+    shader_path.push(filename);
+    let shader_code = fs::read(&shader_path).expect(&format!("Failed to read shader file: {:?}", shader_path));
     let shader_module_info = vk::ShaderModuleCreateInfo {
         s_type: vk::StructureType::SHADER_MODULE_CREATE_INFO,
         p_next: std::ptr::null(),

c33e3840c43cfc139d70c8951c253ddeba6c6f46
MrScripty <TheEnvironmentGuy@protonmail.com>
2025-04-16 15:24:50 -0700

    Cleanup Depreciated Mod

--- rust/src/gui_framework/mod.rs
+++ rust/src/gui_framework/mod.rs
@@ -1,6 +1,5 @@
 pub mod rendering;
 pub mod context;
-pub mod window;
 pub mod interaction;
 pub mod scene {
     pub mod scene;
@@ -11,7 +10,6 @@ pub mod event_bus;
 
 pub use rendering::render_engine::Renderer;
 pub use context::vulkan_context::VulkanContext;
-pub use window::window_handler::VulkanContextHandler;
 pub use scene::scene::{Scene, RenderObject, InstanceData};
 pub use interaction::controller::InteractionController;
 pub use event_bus::{EventBus, BusEvent, EventHandler};
